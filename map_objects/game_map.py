407040ea73cd4f9c1682ba4742d55b4403a40574
MDDay <daymatthewdavid@gmail.com>
2020-05-10 11:47:10 +0100

    Added random map drawing
    rooms are now randomly generated and randomly connected.

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,4 +1,6 @@
 from map_objects.tile import Tile
+from map_objects.rectangle import Rect
+from random import randint
 
 class GameMap:
     def __init__(self, width, height):
@@ -7,19 +9,76 @@ class GameMap:
         self.tiles = self.initialize_tiles()
 
     def initialize_tiles(self):
-        tiles = [[Tile(False) for y in range(self.height)] for x in range(self.width)]
-
-        tiles[30][22].blocked = True
-        tiles[30][22].block_sight = True
-        tiles[31][22].blocked = True
-        tiles[31][22].block_sight = True
-        tiles[32][22].blocked = True
-        tiles[32][22].block_sight = True
-
+        tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
         return tiles
+
+    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
+        rooms = []
+        num_rooms = 0
+
+        for r in range(max_rooms):
+            #generate random room size
+            w = randint(room_min_size, room_max_size)
+            h = randint(room_min_size, room_max_size)
+
+            #randomly place inside map boundries
+            x = randint(0, map_width - w -1)
+            y = randint(0, map_height - h -1)
+
+            #rect handles rectangles
+            new_room = Rect(x, y, w, h)
+
+            #iterate through and see if they collide
+            for other_room in rooms:
+                if new_room.intersect(other_room):
+                    break
+            else:
+                #valid room placement
+                self.create_room(new_room)
+
+                (new_x, new_y) = new_room.center()
+
+                if num_rooms == 0:
+                    #place player in first available room
+                    player.x = new_x
+                    player.y = new_y
+
+                else:
+                    #make sure all other rooms are connected
+                    (prev_x, prev_y) = rooms[num_rooms - 1]. center()
+
+                    #decide if to move horizontally or vertically first
+                    if randint(0, 1) == 1:
+                        # first move horizontally, then vertically
+                        self.create_h_tunnel(prev_x, new_x, prev_y)
+                        self.create_v_tunnel(prev_y, new_y, new_x)
+                    else:
+                        self.create_v_tunnel(prev_y, new_y, prev_x)
+                        self.create_h_tunnel(prev_x, new_x, new_y)
+
+                rooms.append(new_room)
+                num_rooms += 1
+
+    def create_room(self, room):
+        #make passable rooms
+        for x in range(room.x1 + 1, room.x2):
+            for y in range(room.y1 + 1, room.y2):
+                self.tiles[x][y].blocked = False
+                self.tiles[x][y].block_sight = False
+
+    def create_h_tunnel(self, x1, x2, y):
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
+
+    def create_v_tunnel(self, y1, y2, x):
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
 
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
             return True
-
         return False
+
+    
